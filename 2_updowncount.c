1e764461f789fbc58e5c20c15f640c130adb2ffa
Sapthami Upadhya <157310822+sapthamiu@users.noreply.github.com>
2025-02-27 16:46:01 +0530

    Add files via upload

---
+++ 2_updowncount.c
@@ -0,0 +1,30 @@
+#include <LPC17xx.h>
+unsigned int i;
+unsigned long LED;
+int main(void){
+	SystemInit();
+	SystemCoreClockUpdate();
+	
+	LPC_PINCON -> PINSEL0 &= 0xFF0000FF;
+	LPC_PINCON -> PINSEL1 &= 0xFFFFF3FF;
+	LPC_GPIO0 -> FIODIR |= 0x00000FF0;
+	LPC_GPIO0 -> FIODIR &= 0xFFDFFFFF;
+	while(1){
+		if( LPC_GPIO0 -> FIOPIN & 1 << 21){
+			for(LED = 0; LED < 256; LED++){
+				LPC_GPIO0 -> FIOPIN = LED << 4;
+				for(i = 0; i < 100000; i++);
+				if(!( LPC_GPIO0 -> FIOPIN & 1 << 21))
+						break;
+			}
+		}
+		else{
+			for(LED = 255; LED >= 0; LED--){
+				LPC_GPIO0 -> FIOPIN = LED << 4;
+				for(i = 0; i < 100000; i++);
+				if( LPC_GPIO0 -> FIOPIN & 1 << 21)
+						break;
+			}
+		}
+	}
+}
---
+++ 3_RingCount.c
@@ -0,0 +1,18 @@
+#include <LPC17xx.h>
+unsigned int i;
+unsigned long LED = 0x00000010;
+unsigned int place;
+int main(void){
+	LPC_PINCON -> PINSEL0 &= 0xFF0000FF;
+	LPC_GPIO0 -> FIODIR |= 0x00000FF0;
+	LPC_PINCON -> PINSEL1 &= 0xFFFFF3FF;
+	LPC_GPIO0 -> FIODIR &= 0xFFDFFFFF;
+	while(1){
+		if(LPC_GPIO0 ->FIOPIN & 1<<21){
+			for(place = 8; place >= 0; place--){
+				LPC_GPIO0 -> FIOPIN = LED<<place;
+				for(i = 0; i < 100000; i++);
+			}
+		}
+	}
+}
