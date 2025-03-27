b298635094a1e97f8432fde0ce8d1f4b3548c6c4
Sapthami Upadhya <157310822+sapthamiu@users.noreply.github.com>
2025-03-27 16:23:04 +0530

    Add files via upload

---
+++ 1_rotstepswitch.c
@@ -0,0 +1,39 @@
+#include<lpc17xx.h>
+unsigned long int var1, var2;
+unsigned int i = 0, j = 0, k = 0;
+void clockwise(void){
+	var1 = 0x00000008;
+	for(i = 0; i <= 3; i++){
+		var1 <<= 1;
+		LPC_GPIO0->FIOPIN = var1;
+		for(k = 0; k < 3000; k++);
+	}
+}
+void anticlockwise(void){
+	var1 = 0x00000100;
+	for(i = 0; i <= 3; i++){
+		var1 >>= 1;
+		LPC_GPIO0->FIOPIN = var1;
+		for(k = 0; k < 3000; k++);
+	}
+}
+int main(void){
+	SystemInit();
+	SystemCoreClockUpdate();
+	LPC_PINCON->PINSEL0 = 0xFFFF00FF;
+	LPC_GPIO0->FIODIR = 0x000000F0;
+	LPC_PINCON->PINSEL4 &= ~(3 << 24);  //P2.12 as GPIO
+  LPC_GPIO2->FIODIR &= ~(1 << 12);    //P2.12 as input
+	while(1){
+		if(!(LPC_GPIO2->FIOPIN & (1 << 12))){
+			for(j = 0; j < 50; j++)
+				clockwise();
+			for(k = 0; k < 65000; k++);
+		}
+		else{
+			for(j = 0; j < 50; j++)
+				anticlockwise();
+			for(k = 0; k < 65000; k++);
+		}
+	}
+}
